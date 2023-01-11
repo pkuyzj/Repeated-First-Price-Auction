b920c5eec3e1211a4380ebd9a38fe5186312ced0
zjyang <114054750+pkuyzj@users.noreply.github.com>
2023-01-11 20:12:48 +0800

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -6,7 +6,7 @@ e = 2.718282
 def Full_Constrained(T, B, values, hobs, epsilon, delta):
     Budget = B
     rho = (1.0*B)/T
-    K = math.ceil(math.sqrt(T))
+    K = math.ceil(math.sqrt(T)/50)
     M = K
     #N = np.zeros(M) 
     R = np.zeros([M, K])
@@ -52,7 +52,7 @@ def Full_Constrained(T, B, values, hobs, epsilon, delta):
 def Partial_Constrained(T, B, values, hobs, epsilon, delta):
     Budget = B
     rho = (1.0*B)/T
-    K = math.ceil(math.sqrt(T))
+    K = math.ceil(math.sqrt(T)/50)
     M = K
     R = np.zeros([M, K])    # Store the sum. Estimation r = R / n[m][k]
     C = np.zeros([K])       # Store the sum. Estimation c = C / NK[k]
@@ -73,7 +73,7 @@ def Partial_Constrained(T, B, values, hobs, epsilon, delta):
                 R[m][k] += ((m/M)-(k/K))
 
     for t in range(1,T):
-        if t%100==0:
+        if t%1000==0:
             print(t)
         # Get value and hob from data
         value, hob = values[t], hobs[t]
@@ -92,36 +92,25 @@ def Partial_Constrained(T, B, values, hobs, epsilon, delta):
         Lambda = max(Lambda+epsilon*((C[k_t]/NK[k_t])-rho),0)
         
         # Update estimation
-        NM = np.zeros(M)+(T+2023)
-        if hob<=b:
-            # Win in round t. The feedback is one-sided. Only estimators with k>=k_t are updated.
-            for k in range(k_t, K):
-                NK[k] += 1.0
+        NM = np.zeros(M)+(T+2023) #+\infty
+        
+        for k in range(k_t, K):
+            NK[k] += 1.0
+            if k/K>=hob:
                 C[k] += (k/K)*1.0
-            for m in range(M):
-                for k in Active[m]:
-                    if k>= k_t:
-                        n[m][k]+= 1.0
-                        R[m][k]+= (value-(k/K))
-                    NM[m] = min(NM[m], n[m][k]) #!
-        else:
-            # Lost in round t. The feedback is complete. All estimators are updated.
-            for k in range(K):
-                NK[k]+= 1.0
-                if hob <= k/K:
-                    C[k]+= (k/K)*1.0
-            for m in range(M):
-                for k in Active[m]:
-                    n[m][k]+=1.0
-                    if hob <= k/K:
-                        R[m][k] += (value - (k/K))
-                    NM[m] = min(NM[m], n[m][k])
+        for m in range(M):
+            for k in Active[m]:
+                if k>= k_t:
+                    n[m][k]+= 1.0
+                    if k/K>=hob:
+                        R[m][k]+= ((m/M)-(k/K))
+                NM[m] = min(NM[m], n[m][k]) 
 
         # Updating the Active Set for each context.
         bottom = 0  # a variable using to maintain partial-order in elimination.
         CBs = [] #Used in debugging
         for m in range(M): 
-            CB =  math.sqrt((6*(math.log(2*T,e))*(math.log(((M*K*T)/delta), e)))/(NM[m]))#confidence bound
+            CB =  math.sqrt((2*(math.log(((2*M*K*T)/delta), e)))/(NM[m]))#confidence bound
             CBs.append(CB) #Used in debugging
             to_delete = []
             # 1. Find out the "best" arm.
@@ -140,7 +129,14 @@ def Partial_Constrained(T, B, values, hobs, epsilon, delta):
             for k in to_delete:
                 Active[m].remove(k)
             bottom = min(Active[m])
-        #####The following is used in debugging'''
+        #####The following is used in debugging
+        if t%5000 ==0:
+            print(CBs[0])
+            total_size = 0
+            for m in range(M):
+                total_size += len(Active[m])
+            print("Total Active Set Size: "+str(total_size))
+        '''
         if t%500 == 0: 
             for m in range(M):
                 if(m%10 ==0):
@@ -154,7 +150,7 @@ def Partial_Constrained(T, B, values, hobs, epsilon, delta):
 
 def Partial_Unconstrained(T, B, values, hobs, epsilon, delta):
     Budget = B
-    K = math.ceil(math.sqrt(T))
+    K = math.ceil(math.sqrt(T)/50)
     M = K
     R = np.zeros([M, K])    # Store the sum. Estimation r = R / n[m][k]
     n = np.zeros([M, K])    # n_{m, k}
@@ -169,19 +165,17 @@ def Partial_Unconstrained(T, B, values, hobs, epsilon, delta):
                 R[m][k] += ((m/M)-(k/K))
 
     for t in range(1,T):
-        if t%100==0:
+        if t%1000==0:
             print(t)
         # Get value and hob from data
         value, hob = values[t], hobs[t]
-        
         # Locate context m
-        m_t = math.floor(value*M)
+        m_t = math.floor(((value)*M))
         
         # Bid the smallest bid in context M's active set
         k_t = min(Active[m_t])
         b = min(k_t/K, Budget)
-
-        # Update budget, payoff
+        # Update lambda, budget, payoff
         if hob<= b:
             Budget -= b
             Payoff.append(value-b)
@@ -189,28 +183,22 @@ def Partial_Unconstrained(T, B, values, hobs, epsilon, delta):
             Payoff.append(0)
         
         # Update estimation
-        NM = np.zeros(M)+(T+2023)
-        if hob<=b:
-            # Win in round t. The feedback is one-sided. Only estimators with k>=k_t are updated.
-            for m in range(M):
-                for k in Active[m]:
-                    if k>= k_t:
-                        n[m][k]+= 1.0
-                        R[m][k]+= (value-(k/K))
-                    NM[m] = min(NM[m], n[m][k]) #!
-        else:
-            # Lost in round t. The feedback is complete. All estimators are updated.
-            for m in range(M):
-                for k in Active[m]:
-                    n[m][k]+=1.0
-                    if hob <= k/K:
-                        R[m][k] += (value - (k/K))
-                    NM[m] = min(NM[m], n[m][k])
+        NM = np.zeros(M)+(T+2023) #+\infty
+
+        for m in range(M):
+            for k in Active[m]:
+                if k>= k_t:
+                    n[m][k]+= 1.0
+                    if k/K>=hob:
+                        R[m][k]+= ((m/M)-(k/K))
+                NM[m] = min(NM[m], n[m][k]) 
 
         # Updating the Active Set for each context.
         bottom = 0  # a variable using to maintain partial-order in elimination.
+        CBs = [] #Used in debugging
         for m in range(M): 
-            CB =  math.sqrt((6*math.log(2*T,e)*math.log(M*K*T/delta, e))/(NM[m]))#confidence bound
+            CB =  math.sqrt((2*(math.log(((2*M*K*T)/delta), e)))/(NM[m]))#confidence bound
+            CBs.append(CB)
             to_delete = []
             # 1. Find out the "best" arm.
             r_max = -1.0
@@ -228,4 +216,21 @@ def Partial_Unconstrained(T, B, values, hobs, epsilon, delta):
             for k in to_delete:
                 Active[m].remove(k)
             bottom = min(Active[m])
+        #####The following is used in debugging
+        if t%5000 ==0:
+            print(CBs[0])
+            total_size = 0
+            for m in range(M):
+                total_size += len(Active[m])
+            print("Total Active Set Size: "+str(total_size))
+        '''
+        if t%500 == 0: 
+            for m in range(M):
+                if(m%10 ==0):
+                    print('m = {0}, N_m = {1}, CB_m = {2}'.format(m, NM[m], CBs[m]))
+                    for k in range(K):
+                        if(k%10==0):
+                            print(format(R[m][k]/n[m][k], '.4f'), end = ',')
+                    print("")
+        #####'''
     return Payoff
